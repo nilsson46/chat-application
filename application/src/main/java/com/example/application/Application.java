3eebb3d7283c9ccdb87872cbef42ae0236242712
nilsson46 <nilssonsimon46@gmail.com>
2024-02-12 17:18:12 +0100

    Added spring

---
+++ application/src/main/java/com/example/application/Application.java
@@ -0,0 +1,24 @@
+package com.example.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	@RestController
+	static class HelloWorldController {
+
+		@GetMapping("/")
+		public String helloWorld() {
+			return "Hello World!";
+		}
+	}
+
+}
+
