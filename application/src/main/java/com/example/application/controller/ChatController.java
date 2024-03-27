c26d21fb6ac988b2a3bb05b1cd908a1a1ad83843
snilss61 <snilss61@volvocars.com>
2024-03-27 15:49:52 +0100

    Now i can send a message. Not a final solution but just to see that is working

--- application/src/main/java/com/example/application/controller/ChatController.java
+++ application/src/main/java/com/example/application/controller/ChatController.java
@@ -1,6 +1,9 @@
 package com.example.application.controller;
 
+import com.example.application.model.ChatMessage;
 import com.example.application.model.User;
+import com.example.application.service.MessageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -14,38 +17,44 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.List;
+
 @Controller
 public class ChatController {
 
-    User user;
+    @Autowired
+    private MessageService messageService;
 
     @MessageMapping("/chat.sendMessage")
     @SendTo("/topic/public")
-    public ChatMessage sendMessage(
-            @Payload ChatMessage chatMessage
-    ){
+    public ChatMessage sendMessageViaSocket(@Payload ChatMessage chatMessage) {
         return chatMessage;
+    }
+
+    @PostMapping("/chat.sendMessage")
+    public ResponseEntity<ChatMessage> sendMessageViaPost(@RequestBody ChatMessage chatMessage) {
+        ChatMessage createdMessage = messageService.createMessage(chatMessage);
+        return ResponseEntity.ok(createdMessage);
+    }
+
+    @GetMapping("/chat.getMessages")
+    public ResponseEntity<List<ChatMessage>> getMessages() {
+        List<ChatMessage> messages = messageService.getMessages();
+        return ResponseEntity.ok(messages);
     }
 
     @MessageMapping("/chat.addUser")
     @SendTo("/topic/public")
-    public ChatMessage addUser(
-            @Payload ChatMessage chatMessage,
-            SimpMessageHeaderAccessor headerAccessor
-    )
-    {
-        //Add username in web socket session.
+    public ChatMessage addUser(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
         headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
         return chatMessage;
     }
+
     @GetMapping("/join-chat")
     public ResponseEntity<String> joinChat() {
-        // Implementera din logik för att låta användaren gå med i chatten här
-        // Till exempel, spara användarens användarnamn eller ID i en lista med anslutna användare
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String username = authentication.getName();
         String responseMessage = "Ansluten till chatten! Välkommen, " + username + "!";
         return ResponseEntity.ok(responseMessage);
     }
-
 }
